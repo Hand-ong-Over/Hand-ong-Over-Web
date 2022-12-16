c9058dbd89480b9669c55fe2de574f2743ee28a5
양시준 <sijun0905@gmail.com>
2022-12-16 09:18:30 +0900

    BoardService

--- Hand-ong-Over/src/main/java/edu/handong/handongover/board/BoardService.java
+++ Hand-ong-Over/src/main/java/edu/handong/handongover/board/BoardService.java
@@ -1,4 +1,16 @@
 package edu.handong.handongover.board;
 
+import java.util.List;
+
 public interface BoardService {
+    public int insertFindPerson(FindPersonVO vo);
+    public int updateFindPerson(FindPersonVO vo);
+    public int deleteFindPerson(int seq);
+    public FindPersonVO getFindPerson(int seq);
+    public List<FindPersonVO> getFindPersonList();
+    public int insertFindRoom(FindRoomVO vo);
+    public int updateFindRoom(FindRoomVO vo);
+    public int deleteFindRoom(int seq);
+    public FindRoomVO getFindRoom(int seq);
+    public List<FindRoomVO> getFindRoomList();
 }
