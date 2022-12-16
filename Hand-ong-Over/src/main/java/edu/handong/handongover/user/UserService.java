525e4d7c51b33b445bf7a7194341c5bb84dba2cd
양시준 <sijun0905@gmail.com>
2022-12-16 09:49:18 +0900

    UserService and UserServiceImpl

--- Hand-ong-Over/src/main/java/edu/handong/handongover/user/UserService.java
+++ Hand-ong-Over/src/main/java/edu/handong/handongover/user/UserService.java
@@ -1,4 +1,8 @@
 package edu.handong.handongover.user;
 
 public interface UserService {
+    UserVO getUser(UserVO vo);
+    int insertUser(UserVO vo);
+    int updateUser(UserVO vo);
+    int deleteUser(UserVO vo);
 }
--- Hand-ong-Over/src/main/java/edu/handong/handongover/user/UserServiceImpl.java
+++ Hand-ong-Over/src/main/java/edu/handong/handongover/user/UserServiceImpl.java
@@ -1,4 +1,30 @@
 package edu.handong.handongover.user;
 
-public class UserServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    UserDAO userDAO;
+
+    @Override
+    public UserVO getUser(UserVO vo) {
+        return userDAO.getUser(vo);
+    }
+
+    @Override
+    public int insertUser(UserVO vo) {
+        return userDAO.insertUser(vo);
+    }
+
+    @Override
+    public int updateUser(UserVO vo) {
+        return userDAO.updateUser(vo);
+    }
+
+    @Override
+    public int deleteUser(UserVO vo) {
+        return userDAO.deleteUser(vo);
+    }
 }
