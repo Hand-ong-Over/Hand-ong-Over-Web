57ee122cc78a39a02eb2644a171b62497a768f24
양시준 <sijun0905@gmail.com>
2022-12-16 09:58:18 +0900

    LoginController and Interceptor

--- Hand-ong-Over/src/main/java/edu/handong/handongover/user/LoginCheckInterceptor.java
+++ Hand-ong-Over/src/main/java/edu/handong/handongover/user/LoginCheckInterceptor.java
@@ -1,4 +1,32 @@
 package edu.handong.handongover.user;
 
-public class LoginCheckInterceptor {
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginCheckInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        HttpSession session = request.getSession();
+        if (session.getAttribute("userid") == null) {
+            response.sendRedirect(request.getContextPath() + "/login/login");
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        super.afterCompletion(request, response, handler, ex);
+    }
 }
--- Hand-ong-Over/src/main/java/edu/handong/handongover/user/LoginController.java
+++ Hand-ong-Over/src/main/java/edu/handong/handongover/user/LoginController.java
@@ -1,4 +1,45 @@
 package edu.handong.handongover.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
 public class LoginController {
+
+    @Autowired
+    UserServiceImpl userService;
+
+    @RequestMapping(value="/login", method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping(value="/loginOk", method = RequestMethod.POST)
+    public String loginCheck(HttpSession session, UserVO vo) {
+        String returnURL = "";
+        if (session.getAttribute("login") != null) {
+            session.removeAttribute("login");
+        }
+
+        UserVO loginVO = userService.getUser(vo);
+        if (loginVO != null) {
+            System.out.println("로그인 성공");
+            session.setAttribute("login", loginVO);
+            returnURL = "redirect:/board/list";
+        } else {
+            System.out.println("로그인 실패");
+            returnURL = "redirect:/login/login";
+        }
+        return returnURL;
+    }
+
+    @RequestMapping(value="/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/login/login";
+    }
 }
