41d564b3994a4f930dfd5f9c931e299b64c15675
양시준 <sijun0905@gmail.com>
2022-12-16 09:14:27 +0900

    FindRoom VO and DAO

--- Hand-ong-Over/src/main/java/edu/handong/handongover/board/FindRoomDAO.java
+++ Hand-ong-Over/src/main/java/edu/handong/handongover/board/FindRoomDAO.java
@@ -1,4 +1,29 @@
 package edu.handong.handongover.board;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public class FindRoomDAO {
+    @Autowired
+    SqlSession sqlSession;
+
+    public int insertFindRoom(FindRoomVO vo) {
+        return sqlSession.insert("FindRoom.insertFindRoom", vo);
+    }
+    public int deleteFindRoom(int seq) {
+        return sqlSession.delete("FindRoom.deleteFindRoom", seq);
+    }
+    public int updateFindRoom(FindRoomVO vo) {
+        return sqlSession.update("FindRoom.updateFindRoom", vo);
+    }
+    public FindRoomVO getFindRoom(int seq) {
+        return sqlSession.selectOne("FindRoom.getFindRoom", seq);
+    }
+    public List<FindRoomVO> getFindRoomList() {
+        return sqlSession.selectList("FindRoom.getFindRoomList");
+    }
 }
--- Hand-ong-Over/src/main/java/edu/handong/handongover/board/FindRoomVO.java
+++ Hand-ong-Over/src/main/java/edu/handong/handongover/board/FindRoomVO.java
@@ -1,4 +1,132 @@
 package edu.handong.handongover.board;
 
+import java.time.LocalDateTime;
+
 public class FindRoomVO {
+    private int seq;
+    private String writer;
+    private String title;
+    private String start_date;
+    private String end_date;
+    private String room_type;
+    private int price;
+    private int num_of_people;
+    private int sex;
+    private int smoke;
+    private int alcohol;
+    private int pet;
+    private String additional_info;
+    private LocalDateTime reg_date;
+
+    public int getSeq() {
+        return seq;
+    }
+
+    public void setSeq(int seq) {
+        this.seq = seq;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(String start_date) {
+        this.start_date = start_date;
+    }
+
+    public String getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(String end_date) {
+        this.end_date = end_date;
+    }
+
+    public String getRoom_type() {
+        return room_type;
+    }
+
+    public void setRoom_type(String room_type) {
+        this.room_type = room_type;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getNum_of_people() {
+        return num_of_people;
+    }
+
+    public void setNum_of_people(int num_of_people) {
+        this.num_of_people = num_of_people;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public int getSmoke() {
+        return smoke;
+    }
+
+    public void setSmoke(int smoke) {
+        this.smoke = smoke;
+    }
+
+    public int getAlcohol() {
+        return alcohol;
+    }
+
+    public void setAlcohol(int alcohol) {
+        this.alcohol = alcohol;
+    }
+
+    public int getPet() {
+        return pet;
+    }
+
+    public void setPet(int pet) {
+        this.pet = pet;
+    }
+
+    public String getAdditional_info() {
+        return additional_info;
+    }
+
+    public void setAdditional_info(String additional_info) {
+        this.additional_info = additional_info;
+    }
+
+    public LocalDateTime getReg_date() {
+        return reg_date;
+    }
+
+    public void setReg_date(LocalDateTime reg_date) {
+        this.reg_date = reg_date;
+    }
 }
