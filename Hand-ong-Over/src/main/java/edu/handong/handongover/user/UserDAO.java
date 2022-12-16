2d7bc18220d6f1da751cf7716db3ec7fe8f65d68
양시준 <sijun0905@gmail.com>
2022-12-16 09:48:51 +0900

    UserVO and DAO

--- Hand-ong-Over/src/main/java/edu/handong/handongover/user/UserDAO.java
+++ Hand-ong-Over/src/main/java/edu/handong/handongover/user/UserDAO.java
@@ -1,4 +1,24 @@
 package edu.handong.handongover.user;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class UserDAO {
+    @Autowired
+    SqlSessionTemplate sqlSession;
+
+    public UserVO getUser(UserVO vo) {
+        return sqlSession.selectOne("user.getUser", vo);
+    }
+    public int insertUser(UserVO vo) {
+        return sqlSession.insert("user.insertUser", vo);
+    }
+    public int updateUser(UserVO vo) {
+        return sqlSession.update("user.updateUser", vo);
+    }
+    public int deleteUser(UserVO vo) {
+        return sqlSession.delete("user.deleteUser", vo);
+    }
 }
--- Hand-ong-Over/src/main/java/edu/handong/handongover/user/UserVO.java
+++ Hand-ong-Over/src/main/java/edu/handong/handongover/user/UserVO.java
@@ -1,4 +1,31 @@
 package edu.handong.handongover.user;
 
 public class UserVO {
+    private String userid;
+    private String username;
+    private String userpw;
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserpw() {
+        return userpw;
+    }
+
+    public void setUserpw(String userpw) {
+        this.userpw = userpw;
+    }
 }
